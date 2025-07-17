e96d224ff852976e5763844f4c2fcd400c92f774
Selvavinayagam S <145422071+SelvaSardar@users.noreply.github.com>
2025-07-17 15:21:06 +0530

    Add files via upload

---
+++ LoopTask1.js
@@ -0,0 +1,35 @@
+for (let i=1; i<=10; i++)//For Loop is Condition Increment.
+{
+    console.log("The Increment of i is ",i);
+}
+
+for (let i=10; i>=1; i--)//For Loop is Condition Decrement.
+{
+    console.log("The Decrement of i is",i);
+}
+
+let x=1;
+while (x<=10)//While Loop is Condition Increment.
+{
+    console.log("The Whileloop Increment of x is ",x);
+    x++;
+}
+
+let z=10;
+while (z>=1)//While Loop is Condition Decrement.
+{
+    console.log("The Whileloop Decrement of z is ",z);
+    z--;
+}
+
+let y=6;
+do {
+    console.log("The do-while y is ",y);
+    y++;
+}while (y<=9)//The Condition is Put Down Last.
+
+let q=10;
+do {
+    console.log("The do-while q is ",q);
+    q--;
+}while(q>=4)//Decrement.                  
