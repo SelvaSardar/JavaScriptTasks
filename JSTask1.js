313664910c535daa85641d502ed9af3697b013c6
Selvavinayagam S <145422071+SelvaSardar@users.noreply.github.com>
2025-07-17 15:20:24 +0530

    Add files via upload

---
+++ JSTask1.js
@@ -0,0 +1,14 @@
+let a=70;
+let b=20;
+
+let sum=a+b;
+console.log("The sum of  two numbers is "+sum);
+
+let difference=a-b;
+console.log("The difference of two numbers is "+difference);
+
+let product=a*b;
+console.log("The product of two number is "+product);
+
+let quotient=a/b;
+console.log("The quotient of two number is "+quotient);
