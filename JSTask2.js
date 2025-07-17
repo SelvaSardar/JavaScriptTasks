f4a88c3ce8b8f62cbe1d315f8be3dd61323b5002
Selvavinayagam S <145422071+SelvaSardar@users.noreply.github.com>
2025-07-17 19:57:27 +0530

    Add files via upload

---
+++ JSTask2.js
@@ -0,0 +1,13 @@
+let a = 90;
+let b = 60;
+let c = 10;
+
+if ((a > b)&&(a > c)) {
+    console.log("The number is greatest:"+a);
+}
+else if (b > c) {
+    console.log("The number is greatest:"+b);
+}
+else {
+    console.log("The number is greatest:"+c);
+}
---
+++ JSTask3.js
@@ -0,0 +1,9 @@
+let number=30;
+
+if(number%3==0 && number%5==0)
+{
+    console.log(number,"is divisile.");
+}
+else{
+    console.log(number,"is not divisible.");
+}
